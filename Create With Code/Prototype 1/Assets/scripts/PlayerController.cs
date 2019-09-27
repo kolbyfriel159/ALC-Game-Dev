5df9181431b682d0dbb67bd0203a7abdf24599ba
Kolby <kolbyfriel159@gmail.com>
2019-09-27 10:07:30 -0600

    Added Create With Code
    Added create with code unity project

---
+++ Create With Code/Prototype 1/Assets/scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float moveSpeed = 50f;
+    private float turnSpeed = 50f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed * forwardInput);
+
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+       
+      
+
+
+
+
+
+    }
+}
+ 
---
+++ Create With Code/Prototype 1/Assets/scripts/move.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float moveSpeed = 50f;
+    public float turnSpeed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+             transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
+         }
+          else if (Input.GetKey(KeyCode.S))
+          {
+              transform.Translate(Vector3.forward * Time.deltaTime * -moveSpeed);
+          }
+
+          if (Input.GetKey(KeyCode.A))
+          {
+              transform.Rotate(Vector3.up * Time.deltaTime * -turnSpeed);
+
+          }
+           else if (Input.GetKey(KeyCode.D))
+         {
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed);
+
+         }
+
+    }
+}
