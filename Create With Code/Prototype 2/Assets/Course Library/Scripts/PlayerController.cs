67117dd13c1171abaa2ff7f31573525176b8b13c
Kolby <kolbyfriel159@gmail.com>
2019-10-07 10:10:27 -0600

    made new project

---
+++ Create With Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < -20)
+        {
+            transform.position = new Vector3(-20, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 20)
+        {
+            transform.position = new Vector3(20, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+    }
+}
