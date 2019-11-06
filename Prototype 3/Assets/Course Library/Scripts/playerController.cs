47d4270824a852681bccc4586873d89996b514af
Kolby <kolbyfriel159@gmail.com>
2019-11-06 10:28:06 -0700

    sah dude

---
+++ Prototype 3/Assets/Course Library/Scripts/playerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            playerRb = GetComponent<Rigidbody>();
+        // playerRb.AddForce(Vector3.up * 1000);
+        Physics.gravity *= gravityModifier;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
